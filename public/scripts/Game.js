7cac13a4d29189493d70d21021e83697edef5725
Jesse <jessedaniel@live.ca>
2021-07-31 13:59:52 -0600

    add checkForWin function

--- public/scripts/Game.js
+++ public/scripts/Game.js
@@ -16,4 +16,31 @@ export default class Game {
     this.grid[index] = this.turn;
     this.toggleTurn();
   }
+
+  checkForWin() {
+    const possibleWins = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (const win of possibleWins) {
+      const [index1, index2, index3] = win;
+
+      if (
+        this.grid[index1] &&
+        this.grid[index1] === this.grid[index2] &&
+        this.grid[index1] === this.grid[index3]
+      ) {
+        return win;
+      }
+    }
+
+    return null;
+  }
 }
