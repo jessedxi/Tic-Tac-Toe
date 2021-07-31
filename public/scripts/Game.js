d18f95ecc229c191e2d523faa027bfe61abf6f03
Jesse <jessedaniel@live.ca>
2021-07-31 14:26:28 -0600

    alternative checkWin function

--- public/scripts/Game.js
+++ public/scripts/Game.js
@@ -2,6 +2,16 @@ export default class Game {
   constructor() {
     this.turn = "Blue";
     this.grid = new Array(9).fill(null);
+    this.possibleWins = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
   }
 
   toggleTurn() {
@@ -42,5 +52,17 @@ export default class Game {
     }
 
     return null;
+  }
+
+  checkWin() {
+    let combinations = this.grid.reduce(
+      (acc, elem, index) => (elem === this.turn ? acc.concat(index) : acc, [])
+    );
+    let gameWon = null;
+    for (let [i, win] of this.possibleWins.entries()) {
+      if (win.every((element) => combinations.idexOf(element > -1))) {
+        gameWon = { index: i, player: this.turn };
+      }
+    }
   }
 }
