e6521faa6cc735012ca82290783e80d18c1410ba
Jesse <jessedaniel@live.ca>
2021-07-31 16:54:28 -0600

    add board

--- public/scripts/GameDisplay.js
+++ public/scripts/GameDisplay.js
@@ -2,17 +2,24 @@ export default class GameDisplay {
   constructor(root) {
     this.root = root;
     this.root.innerHTML = `
-        <div class="gameHeader">
-          <div class="gameHeader__turnDisplay"
-            Blue's Turn!
+          <div class="header">
+              <div class="gameHeader__turn"> Blue Turn</div>
+              <div class="gameHeader__gameStatus"> In Progress...</div>
+              <button type="button" class="gameHeader__gameRestart">
+                  restart
+              </button>
           </div>
-          <div class="gameHeader__gameStatus">
-            Game in progress...
+          <div class="board">
+              <div class="board__tile" data-index="0"></div>
+              <div class="board__tile" data-index="1"></div>
+              <div class="board__tile" data-index="2"></div>
+              <div class="board__tile" data-index="3"></div>
+              <div class="board__tile" data-index="4"></div>
+              <div class="board__tile" data-index="5"></div>
+              <div class="board__tile" data-index="6"></div>
+              <div class="board__tile" data-index="7"></div>
+              <div class="board__tile" data-index="8"></div>
           </div>
-          <button type="button" class="gameHeader__gameReset">
-            Restart
-          </button>
-        </div>
-    `;
+      `;
   }
 }
