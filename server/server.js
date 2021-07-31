48ba4046fb5df6aeae1d94a1bb0a5ed0bc8f44a7
Jesse <jessedaniel@live.ca>
2021-07-31 13:18:42 -0600

    change to sendFile

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
 
-app.set("view engine", "ejs");
-
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../views", "index.html"));
 });
 
 app.listen(port, () => {
