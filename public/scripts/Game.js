4374ccea88b36298c11522e0117c36e4af0e1e43
Jesse <jessedaniel@live.ca>
2021-07-31 13:35:13 -0600

    add turn toggle function

--- public/scripts/Game.js
+++ public/scripts/Game.js
@@ -3,4 +3,8 @@ export default class Game {
     this.turn = "Blue";
     this.grid = new Array(9).fill(null);
   }
+
+  toggleTurn() {
+    this.turn = this.turn === "Blue" ? "Yellow" : "Blue";
+  }
 }
