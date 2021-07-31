803fab28a3950f61b6bdbdc79a3970f973aee232
Jesse <jessedaniel@live.ca>
2021-07-31 16:54:36 -0600

    add gameDisplay

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,4 +2,4 @@ import Game from "./Game.js";
 import GameDisplay from "./GameDisplay.js";
 
 let game = new Game();
-let GameDisplay = new GameDisplay(document.getElementById("app"));
+let gameDisplay = new GameDisplay(document.getElementById("app"));
