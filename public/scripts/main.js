32536db762f7ef1196b9a519ec44715e2b60fe60
Jesse <jessedaniel@live.ca>
2021-07-31 18:44:38 -0600

    add onTileClick

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,3 +3,9 @@ import GameDisplay from "./GameDisplay.js";
 
 let game = new Game();
 let gameDisplay = new GameDisplay(document.getElementById("app"));
+
+gameDisplay.onTileClick = (index) => {
+  console.log(index);
+};
+
+gameDisplay.onRestart = () => {};
