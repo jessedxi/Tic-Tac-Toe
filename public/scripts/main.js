54c382a8e53c33b53eb4d247d31c5114d41fbb43
Jesse <jessedaniel@live.ca>
2021-08-03 13:10:54 -0600

    create random mode

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,8 +5,16 @@ let game = new Game();
 let gameDisplay = new GameDisplay(document.getElementById("app"));
 
 gameDisplay.onTileClick = (index) => {
-  game.placeColor(index);
-  gameDisplay.update(game);
+  if (gameDisplay.random === "on") {
+    const random = Math.floor(Math.random() * game.playableTiles.length);
+    console.log(random);
+    game.placeColor(game.playableTiles[random]);
+    game.playableTiles.splice(random, 1);
+    gameDisplay.update(game);
+  } else {
+    game.placeColor(index);
+    gameDisplay.update(game);
+  }
 };
 
 gameDisplay.onRestart = () => {
